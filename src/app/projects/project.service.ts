9718bea83fcd43016202460fba19a11ab266cbe2
anguyen2002 <angnguyen2002@gmail.com>
2026-01-15 20:54:52 -0700

    updates

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -10,20 +10,20 @@ export class ProjectService {
     {
       title: 'UCAR Nitrogen Cycle Game',
       description: 'Engineered a multi-lingual web-based educational simulation in collaboration with the University Corporation for Atmospheric Research (UCAR) to model the nitrogen cycle. Implemented component-based architecture, reactive state management, and dynamic UI updates to create an interactive learning experience.',
-      imgURL: '/assets/ncg.png',
+      imgURL: 'assets/ncg.png',
       link: 'https://scied.ucar.edu/interactive/nitrogen-cycle-game',
       techStack: 'Angular, ngx-translate, TypeScript, CSS'
     },
     {
       title: 'Lets Eat',
       description: 'Developed a full-stack mobile application with a focus on real-time data handling and geolocation services, enabling students to discover nearby restaurants and coordinate shared dining experiences. Implemented RESTful APIs, state management, and responsive UI design for seamless user interactions.',
-      imgURL: '/assets/letsEat.png',
+      imgURL: 'assets/letsEat.png',
       techStack: 'React, JavaScript, Figma, RESTful APIs'
     },
     {
       title: 'Song Wordle',
       description: 'Designed and implemented a music-based web game using Angular, leveraging modular components, TypeScript-driven game logic, and SCSS-based styling. Integrated audio processing and interactive gameplay mechanics to provide an engaging user experience inspired by the Wordle framework.',
-      imgURL: '/assets/song.png', 
+      imgURL: 'assets/song.png', 
       techStack: 'Angular, TypeScript, SCSS'
     }
   ]
